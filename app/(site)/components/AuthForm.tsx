9a4c321ba911271c0e75c259ca8d4a581c78131e
Ayesh Biswal <biswalayesh3@gmail.com>
2023-05-27 22:05:55 +0530

    Redirect on login/signup and show empty state

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import axios from "axios";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import Input from "@/app/components/inputs/Input";
@@ -10,13 +10,22 @@ import AuthSocialButton from "./AuthSocialButton";
 
 import { BsGithub, BsGoogle } from 'react-icons/bs';
 import { toast } from 'react-hot-toast';
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from "next/navigation";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 export const AuthForm = () => {
+  const session = useSession();
+  const router = useRouter();
   const [variant, setVariant] = useState<Variant>('LOGIN');
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (session?.status === 'authenticated') {
+      router.push('/users');
+    }
+  }, [session?.status, router]);
 
   const {register, handleSubmit, reset, formState: {errors}} = useForm<FieldValues>({
     defaultValues: {
@@ -43,6 +52,7 @@ export const AuthForm = () => {
 
     if (variant === 'REGISTER') {
       axios.post('/api/register', data)
+      .then(() => signIn('credentials', data))
       .catch(() => toast.error('Something went wrong!'))
       .finally(() => setIsLoading(false));
     }
@@ -59,6 +69,7 @@ export const AuthForm = () => {
 
         if (callback?.ok && !callback?.error) {
           toast.success('Logged in successfully!');
+          router.push('/users');
         }
       })
       .finally(() => setIsLoading(false));
